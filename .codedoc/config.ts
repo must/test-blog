7886ac2f8e626569233ced123cc3fc908495af1b
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2020-05-05 21:26:26 +0200

    cleanup build files from git

--- .codedoc/config.ts
+++ .codedoc/config.ts
@@ -7,6 +7,11 @@ import { theme } from './theme';
 export const config = /*#__PURE__*/configuration({
   theme,                                  // --> add the theme. modify `./theme.ts` for chaning the theme.
   
+  dest: {
+    assets: 'dist',
+    html: 'dist',
+  },
+
   page: {
     title: {
       base: 'Test Blog'                   // --> the base title of your doc pages
